f7de6a81d7f605d65b0e0fd20da5e72ebbce454d
rkagada <rkaganda@gmail.com>
2015-04-09 23:45:02 -0700

    optimized

--- src/generate.js
+++ src/generate.js
@@ -11,35 +11,20 @@ vdata = [
     ];
 
 function subdivide(v1, v2, v3, sphere_points, d) {
-	console.log("depth="+d);
     if(d <= 0) {
-		console.log("sphere_points.length="+sphere_points.length);
-		console.log(v1);
-		console.log(v2);
-		console.log(v3);
         sphere_points.push(v1);
         sphere_points.push(v2);
         sphere_points.push(v3);
         return;
     }
-	/*
-    var v12 = (v1 + v2).norm();
-    var v23 = (v2 + v3).norm();
-    var v31 = (v3 + v1).norm(); 
-	*/
 	d = d -1;
 	var v12 = v1.clone().add(v2).normalize();
     var v23 = v2.clone().add(v3).normalize();
     var v31 = v3.clone().add(v1).normalize();
-	console.log("subdivide");
-	console.log(v12);
-	console.log(v23);
-	console.log(v31);
     subdivide(v1, v12, v31, sphere_points, d);
     subdivide(v2, v23, v12, sphere_points, d);
     subdivide(v3, v31, v23, sphere_points, d);
     subdivide(v12, v23, v31, sphere_points, d);
-	console.log("d end"+d);
 }
 
 function initialize_sphere(sphere_points,depth) {
@@ -51,7 +36,6 @@ function initialize_sphere(sphere_points,depth) {
     ];
 	
     for(var i = 0; i < tindices.length; i++) {
-		console.log("triangle=("+tindices[i][0]+", "+tindices[i][1]+", "+tindices[i][2]+")");
         subdivide(vdata[tindices[i][0]], vdata[tindices[i][1]], vdata[tindices[i][2]], sphere_points, depth);
 	}
 	
@@ -66,13 +50,13 @@ function initVData() {
 }
 
 function generatePoints(depth, radius) {
-	console.log(depth);
 	var sphere_points = new Array();
 	initialize_sphere(sphere_points, depth); // where DEPTH should be the subdivision depth
 	for (var point in sphere_points) {
 		var point_tmp = point * radius;
 	}
 	initSpherePointCloud(sphere_points,0xFF0000);
+	console.log("sphere_points.length="+sphere_points.length);
 }
 
 // Then for the sphere I want to draw, I  iterate over all the precomputed sphere points and with a linear function translate the sphere to its CENTER and chose the proper RADIUS
