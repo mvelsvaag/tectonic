0f5858a36b63e099a6194b4f24030f4166e97299
rkagada <rkaganda@gmail.com>
2015-04-10 06:42:34 -0700

    conrol update

--- gui.js
+++ gui.js
@@ -2,6 +2,13 @@ var generateParams = {
 	depth: 0,
 	radius: 1,
 	scale: 1,
+	sphereColor: 0xffffff,
+	sphereOpacity: 0.1,
+	basePointsColor: 0xFFFFFF,
+	depthVertexColor: 0xFFFF00,
+	faceMeshColor: 0x99CCFF,
+	faceMeshOpacity: 1.0,
+	faceMeshTransparent : false,
 	generate: function() {
 		nScale = generateParams.scale;
 		radius = generateParams.radius;
@@ -18,6 +25,13 @@ function initGUI() {
 
 	gui.add(generateParams, "depth",0,5).step(1);
 	gui.add(generateParams, "scale",1,100).step(1);
-	gui.add(generateParams, "radius",1,10).step(1);;
+	//gui.add(generateParams, "radius",1,10).step(1);;
+	gui.addColor(generateParams, 'sphereColor');
+	gui.add(generateParams, "sphereOpacity",0,1);
+	gui.addColor(generateParams, 'basePointsColor');
+	gui.addColor(generateParams, 'depthVertexColor');
+	gui.addColor(generateParams, 'faceMeshColor');
+	gui.add(generateParams, "sphereOpacity",0,1);
+	gui.add(generateParams, "faceMeshTransparent");
 	gui.add(generateParams, "generate");
 }
