78c19adf3e133c8d0ce52b11620882693bad8707
rkagada <rkaganda@gmail.com>
2015-04-11 10:37:54 -0700

    bugfix code cleanup

--- gui.js
+++ gui.js
@@ -1,6 +1,5 @@
 var daGui;
 var plateGUI;
-
 var generateParams = {
 	depth: 0,
 	radius: 1,
@@ -8,15 +7,15 @@ var generateParams = {
 	sphereMeshEnabled: true,
 	sphereColor: 0xffffff,
 	sphereOpacity: 0.1,
-	sphereTransparent : true,
+	sphereTransparent: true,
 	basePointsColor: 0x000000,
 	basePointsSize: 1,
 	depthVertexColor: 0xFFFFFF,
 	depthVertexSize: 2,
 	faceMeshColor: 0x99CCFF,
 	faceMeshOpacity: 1.0,
-	faceMeshTransparent : false,
-	faceMeshEnabled : false,
+	faceMeshTransparent: false,
+	faceMeshEnabled: false,
 	plateCount: 0,
 	generatePlates: function() {
 		createPlates();
@@ -24,174 +23,171 @@ var generateParams = {
 }
 
 function refresh() {
-	isCalculating=true;
-		if(depthVectors) {
-			removePointCloud(depthVectors);
-			depthVector = null;
-		}
-		nScale = generateParams.scale;
-		radius = generateParams.radius;
-
-		initVData();
-		if(generateParams.sphereMeshEnabled) {
-			updateSphereTemplate();
-		}
-		generatePoints(Number.parseInt(generateParams.depth), radius);
-		updateSpherePointCloud(depthVectors,generateParams.depthVertexColor,generateParams.depthVertexSize);
-		if(generateParams.faceMeshEnabled){
-			initFaces();
-		}
-		if(plateGUI){
-			removeAllPointClouds();
-			createPlates();
-		}
+	isCalculating = true;
+	if (depthVectors) {
+		removePointCloud(depthVectors);
+		depthVector = null;
+	}
+	nScale = generateParams.scale;
+	radius = generateParams.radius;
+	initVData();
+	if (generateParams.sphereMeshEnabled) {
+		updateSphereTemplate();
+	}
+	generatePoints(Number.parseInt(generateParams.depth), radius);
+	updateSpherePointCloud(depthVectors, generateParams.depthVertexColor, generateParams.depthVertexSize);
+	if (generateParams.faceMeshEnabled) {
+		initFaces();
+	}
+	if (plateGUI) {
+		createPlates();
+	}
 }
 
 function createPlates() {
+	if (plates) {
+		removeAllPointClouds();
+	}
 	plates = new Array();
-	for(i=0;i<generateParams.plateCount;i++) {
+	for (i = 0; i < generateParams.plateCount; i++) {
 		//create plate
 		var plate = new Object();
 		plate.vectors = new Array();
-		plate.color = new THREE.Color(1,1,1).getHex(); //default plate color
+		plate.color = new THREE.Color(1, 1, 1).getHex(); //default plate color
 		plate.animateExpand = false;
 		plate.pointSize = 3;
 		plate.vectorCount = "0";
-		plate.expand = function() {
-			expandPlate(plate);
-			updateSpherePointCloud(plate.vectors,plate.color,plate.pointSize);
-		}
 		plates.push(plate);
 	}
 	updateColorFolder();
 }
 
+function plateGUIUpdated(value) {
+	for (var i in plates) {
+		updateSpherePointCloud(plates[i].vectors, plates[i].color, plates[i].pointSize);
+	}
+}
+
 function updateColorFolder() {
 	var p = {
-		seedPlates: function () {
-			for(i in plates) {
-				if(seedPlate(i)) {
-					updateSpherePointCloud(plates[i].vectors,plates[i].color,plates[i].pointSize);
+		seedPlates: function() {
+			for (var i in plates) {
+				if (seedPlate(i)) {
+					updateSpherePointCloud(plates[i].vectors, plates[i].color, plates[i].pointSize);
+				}
+			}
+		},
+		expandPlates: function() {
+			for (var i in plates) {
+				if (expandPlate(i)) {
+					updateSpherePointCloud(plates[i].vectors, plates[i].color, plates[i].pointSize);
 				}
 			}
 		}
 	};
-	if(plateGUI) {
-		plateGUI.destroy();
+	if (plateGUI) {
+		try {
+			plateGUI.destroy();
+		} catch (err) {}
 	}
-	plateGUI = new dat.GUI();
-	plateGUI.add(p, "seedPlates");
-	for(i in plates) { //for each plate
-		var	sizeController = plateGUI.add(plates[i], 'vectorCount');
-		var colorController = plateGUI.addColor(plates[i], 'color');
-		var sizeController = plateGUI.add(plates[i], 'pointSize',1,5).step(1);
-		var expandPlate = plateGUI.add(plates[i], 'expand');
-		
-		colorController.onChange(function(value) {
-			if(this.object.vectors) {
-				updateSpherePointCloud(this.object.vectors,this.object.color,this.object.pointSize);
-			}
-		});
-		
-		sizeController.onChange(function(value) {
-			if(this.object.vectors) {
-				updateSpherePointCloud(this.object.vectors,this.object.color,this.object.pointSize);
-			}
-		});
+	if (plates.length > 0) {
+		plateGUI = new dat.GUI();
+		plateGUI.add(p, "seedPlates");
+		plateGUI.add(p, "expandPlates");
+		for (var i in plates) { //for each plate
+			plateGUI.add(plates[i], 'vectorCount');
+			var colorController = plateGUI.addColor(plates[i], 'color');
+			var sizeController = plateGUI.add(plates[i], 'pointSize', 1, 5).step(1);
+			colorController.onChange(function() {
+				plateGUIUpdated()
+			});
+			sizeController.onChange(function() {
+				plateGUIUpdated()
+			});
+		}
 	}
 }
 
 function updateGUIs() {
-	if(daGui) {
-		 for (var i in daGui.__controllers) {
+	if (daGui) {
+		for (var i in daGui.__controllers) {
 			daGui.__controllers[i].updateDisplay();
 		}
 	}
-	if(plateGUI) {
-		 for (var i in plateGUI.__controllers) {
-			plateGUI.__controllers[i].updateDisplay();
+	if (plateGUI) {
+		for (var j in plateGUI.__controllers) {
+			plateGUI.__controllers[j].updateDisplay();
 		}
 	}
 }
 
 function initGUI() {
 	daGui = new dat.GUI();
-	
-	var depthController = daGui.add(generateParams, "depth",0,5).step(1);
-	var scaleController = daGui.add(generateParams, "scale",1,5).step(1);
+	var depthController = daGui.add(generateParams, "depth", 0, 5).step(1);
+	var scaleController = daGui.add(generateParams, "scale", 1, 5).step(1);
 	var sphereFolder = daGui.addFolder('sphere settings');
 	var sphereController = sphereFolder.add(generateParams, 'sphereMeshEnabled');
 	var sphereColorController = sphereFolder.addColor(generateParams, 'sphereColor');
-	var sphereOpacityController = sphereFolder.add(generateParams, "sphereOpacity",0,1);
+	var sphereOpacityController = sphereFolder.add(generateParams, "sphereOpacity", 0, 1);
 	var sphereTransparentController = sphereFolder.add(generateParams, "sphereTransparent");
-	
 	var depthVectorFolder = daGui.addFolder('depthVector settings');
 	var depthColorController = depthVectorFolder.addColor(generateParams, 'depthVertexColor');
-	var depthSizeController = depthVectorFolder.add(generateParams, 'depthVertexSize',1,5).step(1);
-	
+	var depthSizeController = depthVectorFolder.add(generateParams, 'depthVertexSize', 1, 5).step(1);
 	var faceMeshFolder = daGui.addFolder('faceMesh settings');
 	var faceController = faceMeshFolder.add(generateParams, 'faceMeshEnabled');
 	var faceMeshColorController = faceMeshFolder.addColor(generateParams, 'faceMeshColor');
 	daGui.add(generateParams, "generatePlates");
-	var plateCountController = daGui.add(generateParams, "plateCount",0,5).step(1);
-
-	
+	var plateCountController = daGui.add(generateParams, "plateCount", 0, 5).step(1);
 	/*
-	* gui callbacks
-	* holy callbacks batman
-	*/
+	 * gui callbacks
+	 * holy callbacks batman
+	 */
 	depthController.onChange(function(value) {
 		refresh();
 	});
-	
 	scaleController.onChange(function(value) {
 		refresh();
 	});
-
 	sphereController.onChange(function(value) {
 		updateSphereTemplate();
 	});
 	sphereColorController.onChange(function(value) {
 		initSphereTemplate();
-	});	
+	});
 	sphereOpacityController.onChange(function(value) {
 		initSphereTemplate();
 	});
 	sphereTransparentController.onChange(function(value) {
 		initSphereTemplate();
 	});
-	
 	depthColorController.onChange(function(value) {
-		updateSpherePointCloud(depthVectors,generateParams.depthVertexColor,generateParams.depthVertexSize);
+		updateSpherePointCloud(depthVectors, generateParams.depthVertexColor, generateParams.depthVertexSize);
 	});
 	depthSizeController.onChange(function(value) {
-		updateSpherePointCloud(depthVectors,generateParams.depthVertexColor,generateParams.depthVertexSize);
+		updateSpherePointCloud(depthVectors, generateParams.depthVertexColor, generateParams.depthVertexSize);
 	});
-	
 	faceMeshColorController.onChange(function(value) {
-		if(!depthFaces) {
+		if (!depthFaces) {
 			refresh();
-		}else {
+		} else {
 			initFaces();
-			if(!generateParams.faceMeshEnabled){
+			if (!generateParams.faceMeshEnabled) {
 				document.getElementById("faces").innerHTML = 0;
 				scene.remove(depthFaces);
 			}
 		}
 	});
-	
 	faceController.onChange(function(value) {
-		if(!depthFaces) {
+		if (!depthFaces) {
 			refresh();
-		}else {
+		} else {
 			initFaces();
-			if(!generateParams.faceMeshEnabled){
+			if (!generateParams.faceMeshEnabled) {
 				document.getElementById("faces").innerHTML = 0;
 				scene.remove(depthFaces);
 			}
 		}
 	});
-	
 	plateCountController.onChange(function(value) {
 		createPlates();
 	});
--- src/generate.js
+++ src/generate.js
@@ -4,211 +4,228 @@ var Z = 0.850650808352039932;
 var radius = 1;
 var vdata, tindices;
 var depthTriangles;
-var colorFolder; 
+var colorFolder;
 var plateColorCount = 0; //color folder hack
-
 //var duplicateVertexCount;
 //var duplicateTriangeCount;
-
 vdata = [
-        new THREE.Vector3(-X, 0.0, Z), new THREE.Vector3( X, 0.0, Z ), new THREE.Vector3( -X, 0.0, -Z ), new THREE.Vector3( X, 0.0, -Z ),
-        new THREE.Vector3( 0.0, Z, X ), new THREE.Vector3( 0.0, Z, -X ), new THREE.Vector3( 0.0, -Z, X ), new THREE.Vector3( 0.0, -Z, -X ),
-        new THREE.Vector3( Z, X, 0.0 ), new THREE.Vector3( -Z, X, 0.0 ), new THREE.Vector3( Z, -X, 0.0 ), new THREE.Vector3( -Z, -X, 0.0 )
-    ];
+	new THREE.Vector3(-X, 0.0, Z), new THREE.Vector3(X, 0.0, Z), new THREE.Vector3(-
+		X, 0.0, -Z), new THREE.Vector3(X, 0.0, -Z),
+	new THREE.Vector3(0.0, Z, X), new THREE.Vector3(0.0, Z, -X), new THREE.Vector3(
+		0.0, -Z, X), new THREE.Vector3(0.0, -Z, -X),
+	new THREE.Vector3(Z, X, 0.0), new THREE.Vector3(-Z, X, 0.0), new THREE.Vector3(
+		Z, -X, 0.0), new THREE.Vector3(-Z, -X, 0.0)
+];
 
 function subdivide(v1, v2, v3, sphere_points, d) {
-    if(d == 0) {
+	if (d === 0) {
 		/*
-		*	This entire block is pretty inefficient at the moment.
-		*
-		*	The same vector are iterated over multiple times for each triangle.
-		*	addAdjectProperty and containsVector are called each time.
-		*	The first to add a property adjacent to each vector3 if it doesn't exist and
-		*	the second to check and see if the vector3 needs to be added to sphere_points
-		*	
-		*	Both of these can be done when the vector3s are created.
-		*	This happens in two locations. 
-		*	In here [subdivide] when each vector3s is cloned before a subdivide and
-		* 	in initVData where the initial vector3s are created.
-		*
-		*/
+		 *	This entire block is pretty inefficient at the moment.
+		 *
+		 *	The same vector are iterated over multiple times for each triangle.
+		 *	addAdjectProperty and containsVector are called each time.
+		 *	The first to add a property adjacent to each vector3 if it doesn't exist and
+		 *	the second to check and see if the vector3 needs to be added to sphere_points
+		 *
+		 *	Both of these can be done when the vector3s are created.
+		 *	This happens in two locations.
+		 *	In here [subdivide] when each vector3s is cloned before a subdivide and
+		 *	in initVData where the initial vector3s are created.
+		 *
+		 */
 		//add the vectors to sphere_points if they aren't already there
-		if(containsVector(v1,sphere_points)) {
+		if (containsVector(v1, sphere_points)) {
 			v1.plate = null;
 			sphere_points.push(v1);
 		}
-		if(containsVector(v2,sphere_points)) {
+		if (containsVector(v2, sphere_points)) {
 			v1.plate = null;
 			sphere_points.push(v2);
 		}
-		if(containsVector(v3,sphere_points)) {
+		if (containsVector(v3, sphere_points)) {
 			v1.plate = null;
 			sphere_points.push(v3);
 		}
-		
 		//add the property adjacent if it doesn't exist
 		addAdjectProperty(v1);
-		if(containsVector(v2,v1.adjacent)) {
+		if (containsVector(v2, v1.adjacent)) {
 			v1.adjacent.push(v2);
 		}
-		
-		if(containsVector(v3,v1.adjacent)) {
+		if (containsVector(v3, v1.adjacent)) {
 			v1.adjacent.push(v3);
 		}
-		
 		addAdjectProperty(v2);
-		if(containsVector(v1,v2.adjacent)) {
+		if (containsVector(v1, v2.adjacent)) {
 			v2.adjacent.push(v1);
 		}
-		if(containsVector(v3,v2.adjacent)) {
+		if (containsVector(v3, v2.adjacent)) {
 			v2.adjacent.push(v3);
 		}
-		
 		addAdjectProperty(v3);
-		if(containsVector(v1,v3.adjacent)) {
+		if (containsVector(v1, v3.adjacent)) {
 			v3.adjacent.push(v1);
 		}
-		if(containsVector(v2,v3.adjacent)) {
+		if (containsVector(v2, v3.adjacent)) {
 			v3.adjacent.push(v2);
 		}
 		/*
-		*	end inefficient 
-		*
-		*/
-		
-		var triangle = new THREE.Triangle(v1,v2,v3);
-		triangle.set(v1,v2,v3);
-		if(pushTriangle(triangle)) { //always true (efficient);
+		 *	end inefficient
+		 *
+		 */
+		var triangle = new THREE.Triangle(v1, v2, v3);
+		triangle.set(v1, v2, v3);
+		if (pushTriangle(triangle)) { //always true (efficient);
 			depthTriangles.push(triangle);
 		}
-        return;
-    }
-	d = d -1;
+		return;
+	}
+	d = d - 1;
 	var v12 = v1.clone().add(v2).normalize();
-    var v23 = v2.clone().add(v3).normalize();
-    var v31 = v3.clone().add(v1).normalize();
-    subdivide(v1, v12, v31, sphere_points, d);
-    subdivide(v2, v23, v12, sphere_points, d);
-    subdivide(v3, v31, v23, sphere_points, d);
-    subdivide(v12, v23, v31, sphere_points, d);
+	var v23 = v2.clone().add(v3).normalize();
+	var v31 = v3.clone().add(v1).normalize();
+	subdivide(v1, v12, v31, sphere_points, d);
+	subdivide(v2, v23, v12, sphere_points, d);
+	subdivide(v3, v31, v23, sphere_points, d);
+	subdivide(v12, v23, v31, sphere_points, d);
 }
 
 function addAdjectProperty(v) {
-	if(typeof v.adjacent =="undefined") {
+	if (typeof v.adjacent == "undefined") {
 		v.adjacent = new Array();
 	}
 }
 
-function containsVector(v,vectors) {
-	var contains = false;
-	function checkVector(i) {
-		if(i<vectors.length) {
-			if(v.equals(vectors[i])) {
-				//duplicateVertexCount++;
-				contains =true;
-			}else {
-				checkVector(i+1);
+function containsVector(v, vectors) {
+		var contains = false;
+
+		function checkVector(i) {
+			if (i < vectors.length) {
+				if (v.equals(vectors[i])) {
+					//duplicateVertexCount++;
+					contains = true;
+				} else {
+					checkVector(i + 1);
+				}
 			}
 		}
+		checkVector(0);
+		if (!contains) {
+			return v;
+		}
 	}
-	checkVector(0);
-	if(!contains) {
-		 return v;
-	}
-}
+	//pretty sure this never gets called
+	//its a just in case
 
-//pretty sure this never gets called
-//its a just in case
 function pushTriangle(triangle) {
 	var contains = false;
+
 	function checkFace(i) {
-		if(i<depthTriangles.length) {
-			if(depthTriangles[i]!=null) {
-				if(triangle.equals(depthTriangles[i])) {
+		if (i < depthTriangles.length) {
+			if (depthTriangles[i] !== null) {
+				if (triangle.equals(depthTriangles[i])) {
 					//duplicateTriangeCount++;
-					contains =true;
+					contains = true;
 				}
-			}else {
-				checkFace(i+1);
+			} else {
+				checkFace(i + 1);
 			}
 		}
 	}
 	checkFace(0);
-	if(!contains) {
-		 return triangle;
+	if (!contains) {
+		return triangle;
 	}
 }
 
-
-function initialize_sphere(sphere_points,depth) {
-    tindices = [
-	[0, 4, 1], [0, 9, 4 ],[ 9, 5, 4 ], [ 4, 5, 8 ], [ 4, 8, 1 ],
-        [ 8, 10, 1 ], [ 8, 3, 10 ], [ 5, 3, 8 ], [ 5, 2, 3 ], [ 2, 7, 3 ],
-        [ 7, 10, 3 ], [ 7, 6, 10 ], [ 7, 11, 6 ], [ 11, 0, 6 ],[ 0, 1, 6 ],
-        [ 6, 1, 10 ], [ 9, 0, 11 ], [ 9, 11, 2 ], [ 9, 2, 5 ], [ 7, 2, 11 ]
-    ];
-	
-    for(var i = 0; i < tindices.length; i++) {
-        subdivide(vdata[tindices[i][0]], vdata[tindices[i][1]], vdata[tindices[i][2]], sphere_points, depth);
+function initialize_sphere(sphere_points, depth) {
+	tindices = [
+		[0, 4, 1],
+		[0, 9, 4],
+		[9, 5, 4],
+		[4, 5, 8],
+		[4, 8, 1],
+		[8, 10, 1],
+		[8, 3, 10],
+		[5, 3, 8],
+		[5, 2, 3],
+		[2, 7, 3],
+		[7, 10, 3],
+		[7, 6, 10],
+		[7, 11, 6],
+		[11, 0, 6],
+		[0, 1, 6],
+		[6, 1, 10],
+		[9, 0, 11],
+		[9, 11, 2],
+		[9, 2, 5],
+		[7, 2, 11]
+	];
+	for (var i = 0; i < tindices.length; i++) {
+		subdivide(vdata[tindices[i][0]], vdata[tindices[i][1]], vdata[tindices[i][2]],
+			sphere_points, depth);
 	}
-	
 }
 
 function initVData() {
-	vdata = [
-        new THREE.Vector3(-X, 0.0, Z), new THREE.Vector3( X, 0.0, Z ), new THREE.Vector3( -X, 0.0, -Z ), new THREE.Vector3( X, 0.0, -Z ),
-        new THREE.Vector3( 0.0, Z, X ), new THREE.Vector3( 0.0, Z, -X ), new THREE.Vector3( 0.0, -Z, X ), new THREE.Vector3( 0.0, -Z, -X ),
-        new THREE.Vector3( Z, X, 0.0 ), new THREE.Vector3( -Z, X, 0.0 ), new THREE.Vector3( Z, -X, 0.0 ), new THREE.Vector3( -Z, -X, 0.0 )
-    ]
-}
+		vdata = [
+			new THREE.Vector3(-X, 0.0, Z), new THREE.Vector3(X, 0.0, Z), new THREE.Vector3(-
+				X, 0.0, -Z), new THREE.Vector3(X, 0.0, -Z),
+			new THREE.Vector3(0.0, Z, X), new THREE.Vector3(0.0, Z, -X), new THREE.Vector3(
+				0.0, -Z, X), new THREE.Vector3(0.0, -Z, -X),
+			new THREE.Vector3(Z, X, 0.0), new THREE.Vector3(-Z, X, 0.0), new THREE.Vector3(
+				Z, -X, 0.0), new THREE.Vector3(-Z, -X, 0.0)
+		]
+	}
+	/*
+	 *	Generate entry point.
+	 *
+	 */
 
-/*
-*	Generate entry point.
-*
-*/
 function generatePoints(depth, radius) {
-	var sphere_points = new Array();
-	duplicateVertexCount = 0;
-	duplicateTriangeCount = 0;
-	depthTriangles = new Array();
-	initialize_sphere(sphere_points, depth); // where DEPTH should be the subdivision depth
-	depthVectors = sphere_points;
-	document.getElementById("vertices").innerHTML = sphere_points.length;
-}
+		var sphere_points = new Array();
+		duplicateVertexCount = 0;
+		duplicateTriangeCount = 0;
+		depthTriangles = new Array();
+		initialize_sphere(sphere_points, depth); // where DEPTH should be the subdivision depth
+		depthVectors = sphere_points;
+		document.getElementById("vertices").innerHTML = sphere_points.length;
+	}
+	/*
+	 *	Expand plates.
+	 *	If the vector is part of a plate checks if the adjacent vectors have plates.
+	 *	If they do not they are assigned the starting vectors plates.
+	 *	This function is not recursive and iterates through depthVector once.
+	 *
+	 */
 
-
-/*
-*	Expand plates.
-*	If the vector is part of a plate checks if the adjacent vectors have plates.
-*	If they do not they are assigned the starting vectors plates.
-*	This function is not recursive and iterates through depthVector once.
-*
-*/
-function expandPlate(plate) {
-	for(i in plate.vectors) { //for each vector
-		for(j in plate.vectors[i].adjacent) { //for each adjacent vector 
-			if(typeof plate.vectors[i].adjacent[j].plate=="undefined") { //if vector is not part of a plate
-				plate.vectors[i].adjacent[j].plate = plate.vectors[i].plate; //add the vector to the plate
-				plate.vectors[i].plate.vectors.push(plate.vectors[i].adjacent[j]); //add reference to the plate in the vector
-				plate.vectors[i].plate.vectorCount = plate.vectors[i].plate.vectors.length; //update the count
+function expandPlate(n) {
+		if (plates[n]) {
+			var plate = plates[i];
+			for (var i in plate.vectors) { //for each vector
+				for (var j in plate.vectors[i].adjacent) { //for each adjacent vector 
+					if (typeof plate.vectors[i].adjacent[j].plate == "undefined") { //if vector is not part of a plate
+						plate.vectors[i].adjacent[j].plate = plate.vectors[i].plate; //add the vector to the plate
+						plate.vectors[i].plate.vectors.push(plate.vectors[i].adjacent[j]); //add reference to the plate in the vector
+						plate.vectors[i].plate.vectorCount = plate.vectors[i].plate.vectors.length; //update the count
+					}
+				}
 			}
 		}
 	}
-}
+	/*
+	 *	creates the initial plate seeds
+	 *
+	 */
 
-/*
-*	creates the initial plate seeds
-*
-*/
 function seedPlate(i) {
-	if(plates[i]) {
-	var num = Number.parseInt(Math.random()* depthVectors.length);
-	if(containsVector(depthVectors[num],plates[i].vectors)) {
-		plates[i].vectors.push(depthVectors[num]); //add reference to the vector in the plate
-		plates[i].vectorCount = plates[i].vectors.length; //update the count
-		depthVectors[num].plate = plates[i]; //add reference to the plate in the vector
-	}
-	return true;
+	if (plates[i]) {
+		var num = Number.parseInt(Math.random() * depthVectors.length);
+		if (containsVector(depthVectors[num], plates[i].vectors)) {
+			plates[i].vectors.push(depthVectors[num]); //add reference to the vector in the plate
+			plates[i].vectorCount = plates[i].vectors.length; //update the count
+			depthVectors[num].plate = plates[i]; //add reference to the plate in the vector
+		}
+		return true;
 	}
 	return false;
 }
